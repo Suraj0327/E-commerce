b5e33013e3593cade4de12024a45ee24e44f9028
Suraj0327 <suraj.202074101@vcet.edu.in>
2023-08-07 13:19:52 +0530

    second commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 const userModel = require("../models/userModel");
+const hashPassword=require("../helpers/authHelper");
+const jwt =require("jsonwebtoken");
+const comparePassword = require("../helpers/authHelper");
 
 const registerController = async(req,res) => {
     try{
@@ -18,7 +21,7 @@ const registerController = async(req,res) => {
         if(!address){
             return res.send({error:"address is required"})
         }
-        const existintuser=await userModel.findOne({emial})
+        const existintuser=await userModel.findOne({email})
         if(existintuser){
             return res.status(200).send({
                 success:true,
@@ -27,7 +30,7 @@ const registerController = async(req,res) => {
             })
         }
         const hashedPassword=await hashPassword(password)
-        const user=new userModel({name,email,phone,address,password:hashedPassword}).save()
+        const user= await new userModel({name,email,phone,address,password:hashedPassword}).save()
         res.status(201).send({
             success:true,
             message:"user registed successfully",
@@ -46,4 +49,57 @@ const registerController = async(req,res) => {
   };
   
   module.exports = registerController;
-  
+
+  //login controller
+  const loginController = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        // Validation
+        if (!email || !password) {
+            return res.status(400).send({
+                success: false,
+                message: 'Invalid email or password',
+            });
+        }
+        
+        // Check user
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(404).send({
+                success: false,
+                message: 'Email is not found',
+            });
+        }
+
+        const match = await comparePassword(password, user.password); // Assuming you have implemented comparePassword
+
+        if (!match) {
+            return res.status(401).send({
+                success: false,
+                message: 'Invalid password',
+            });
+        }
+
+        const token = await jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7 days' });
+
+        res.status(200).send({
+            success: true,
+            message: 'Login successful',
+            user: {
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                address: user.address,
+            },
+            token,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({
+            success: false,
+            message: 'Error in login',
+            error,
+        });
+    }
+};
+module.exports=loginController
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,9 @@
 const express=require('express');
 const registerController=require('../controllers/authController');
+const loginController = require('../controllers/authController');
 const router=express.Router()
-router.post('/register',registerController)
+//registration post
+router.post('/register',registerController);
+router.post('/login',loginController)
 module.exports=router;
+//login post
